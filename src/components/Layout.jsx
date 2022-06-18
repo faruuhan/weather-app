7839642c44b5bcd4d8b823a6ca09ef8c83a73ff9
farhan <farhnn.r@gmail.com>
2022-06-18 14:04:48 +0700

    tailwind form

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Layout = (props) => {
-  return <main className="bg-slate-100 min-h-[100vh]">{props.children}</main>;
+  return <main className="bg-gradient-to-r from-[#A0BEF8] to-[#B5F0F0] min-h-[100vh]">{props.children}</main>;
 };
 
 export default Layout;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,7 +4,9 @@ import Layout from "../components/Layout";
 const Homepage = () => {
   return (
     <Layout>
-      <div className="container px-4 md:mx-auto">test</div>
+      <div className="container px-4 md:px-0 md:mx-auto">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed ratione exercitationem suscipit. Nemo delectus consequatur, nostrum animi perferendis ipsum natus expedita iste officia adipisci quia totam? Vitae aspernatur nobis in.
+      </div>
     </Layout>
   );
 };
